472d5fb7d16245d3e141c578acb812c56a22c48f
Fuczek <51419805+Fuczek@users.noreply.github.com>
2023-08-14 21:34:15 +0200

    Add files via upload

---
+++ rpm_curve.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var rpm_curve : Curve
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rpm_curve.min_value = 100
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
